a1e93efe1b93dc5c3908a3aa0dfb9453c425324a
duc do <tvietduc122@gmail.com>
2022-10-23 08:04:05 +0700

    create template login page

--- js/register.js
+++ js/register.js
@@ -1,20 +1,21 @@
-function handleRegister() {
-    // 1. lay value tren form
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    // 2. gui value from client to serve
+async function handleRegister() {
+    try {
+         // 1. lay value tren form
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        // 2. gui value from client to serve
 
-    axios.post('http://localhost:5000/api/auth/register', {
-        username: username,
-        email: email,
-        password: password
-    })
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+        const response = await axios.post('http://localhost:5000/api/auth/register', {
+            username: username,
+            email: email,
+            password: password
+        });
+        if(response.status == 200) {
+            window.location.href = "/login.html";
+        }
+    } catch (error) {
+        // log error
+    }
 
 }
