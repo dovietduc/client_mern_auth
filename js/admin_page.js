635b558fcc09b7be3c69156f63d5251bc5d7cc13
duc do <tvietduc122@gmail.com>
2022-10-28 22:44:25 +0700

    format code send

--- js/admin_page.js
+++ js/admin_page.js
@@ -1,9 +1,21 @@
-// call api get list user
-axios.get('http://localhost:5000/auth/admin/user', {
-    headers: {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+async function getListUser() {
+    try {
+       // call api get list user
+        const configHeader = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        };
+        const response = await axios.get('http://localhost:5000/auth/admin/user', configHeader); 
+    } catch (error) {
+        // errror
+        // call refresh token
+        // token expired->redirect to admin
     }
-})
+    
+}
+
+getListUser();
 
 
 
