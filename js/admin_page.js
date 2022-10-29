9dc43408547f604e5200f2d0e4e1e417041d2d38
duc do <tvietduc122@gmail.com>
2022-10-29 09:40:12 +0700

    show list user client

--- js/admin_page.js
+++ js/admin_page.js
@@ -7,6 +7,8 @@ async function getListUser() {
             }
         };
         const response = await axios.get('http://localhost:5000/auth/admin/user', configHeader); 
+        showListUser(response);
+
     } catch (error) {
         // errror
         // call refresh token
@@ -15,8 +17,33 @@ async function getListUser() {
     
 }
 
+function showListUser(response) {
+    let htmlUser = `<table class="table table-hover text-nowrap">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Role</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+    response.data.forEach(function(user, index){
+        htmlUser += `<tr>
+                        <td>${index + 1}</td>
+                        <td>${user.username}</td>
+                        <td>${user.role}</td>
+                        <td>${user.email}</td>
+                        <td>
+                            Delete
+                        </td>
+                    </tr>`
+    });
+    htmlUser += `   </tbody>
+                </table>`;
+    document.querySelector('.list_user').innerHTML = htmlUser;
+    
+}
+
 getListUser();
-
-
-
-// show list user to client
