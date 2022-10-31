47eef8306df93bc668329268438a8ccccc25cd55
duc do <tvietduc122@gmail.com>
2022-10-31 21:52:06 +0700

    send api

---
+++ js/create_user.js
@@ -0,0 +1,23 @@
+async function handleSubmitAddUser() {
+    try {
+        // 1. lay value tren form
+       const username = document.getElementById('username').value;
+       const email = document.getElementById('email').value;
+       const password = document.getElementById('password').value;
+       const role = document.getElementById('role').value;
+       // 2. gui value from client to serve
+
+       const response = await axios.post('api/auth/user/create', {
+           username: username,
+           email: email,
+           password: password,
+           role: role
+       });
+       if(response.status === 200) {
+          
+       }
+   } catch (error) {
+       // log error
+   }
+
+}
